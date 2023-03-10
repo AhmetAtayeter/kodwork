43f8afdc633fb928942788867612bea9b03b8fb8
AhmetAtayeter <95136882+AhmetAtayeter@users.noreply.github.com>
2023-03-10 08:59:49 +0300

    commit 2

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -1,14 +1,30 @@
-import React from "react";
-import { View,Text } from "react-native";
+import React, { useEffect } from "react";
+import { SafeAreaView,Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useDispatch, useSelector } from "react-redux";
 
-const Favorites = () => {
+//AsyncStorage.getItem('@FAVORITES', favorites)
 
+const Favorites = async () => {
+
+    //const dispatch = useDispatch()
+    useEffect(() => {
+        //const  {favorites} = useSelector(s => s.favorites)
+        const favs = () => dispatch({type: 'SET_FAV'})
+        if (favs !== null) {
+            console.log("favs")
+            console.log(favs)
+        }
+    }, [])
+    
+    //const  {favorites} = useSelector(s => s.favorites)
+    // console.log(favorites)
     return(
-        <View>
+        <SafeAreaView>
             <Text>
-                Favorites
+                {/* {favorites} */}
             </Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
